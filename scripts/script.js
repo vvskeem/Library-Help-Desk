0e2bd5fd5dd188d117118e8ce5b6211c9441c176
Shakeem Albouy <albouyshakeem@gmail.com>
2019-12-06 00:15:21 -0400

    heroku Push

--- scripts/script.js
+++ scripts/script.js
@@ -1,206 +1,206 @@
-//  const API = "https://library-sa.herokuapp.com"
-const API = "http://localhost:3000"
+  const API = "https://library-sa.herokuapp.com"
+  // const API = "http://localhost:3000"
 
-$("body").on("click", "#dbBtn", function (event) {
-    // get the user input value
-    event.preventDefault()
-    let user_search = $("#user_text").val()
+  $("body").on("click", "#dbBtn", function (event) {
+      // get the user input value
+      event.preventDefault()
+      let user_search = $("#user_text").val()
 
-    // clear the search bar
-    $("#user_text").val("")
+      // clear the search bar
+      $("#user_text").val("")
 
-    // search for the user input value
-    // localhost:3000/search?term=user_search
-    // whatever we get, console log
-    let searchUrl = API + "/search?term=" + user_search;
-    $.get(searchUrl, function (response) {
-        createBookCards(response.books)
-    })
-})
+      // search for the user input value
+      // localhost:3000/search?term=user_search
+      // whatever we get, console log
+      let searchUrl = API + "/search?term=" + user_search;
+      $.get(searchUrl, function (response) {
+          createBookCards(response.books)
+      })
+  })
 
-// Make a request to our GET /authors route and console.log the results
-let authorUrl = API + "/authors"
-$.get(authorUrl, function (response) {
-    console.log(response)
-    let data = response.result
+  // Make a request to our GET /authors route and console.log the results
+  let authorUrl = API + "/authors"
+  $.get(authorUrl, function (response) {
+      console.log(response)
+      let data = response.result
 
-    // Build cards for each other in the response
-    // Each card will have the following
-    // an image for the avatar
-    // the author full name
-    // the bio
+      // Build cards for each other in the response
+      // Each card will have the following
+      // an image for the avatar
+      // the author full name
+      // the bio
 
-    for (let index = 0; index < data.length; index++) {
-        const element = data[index];
-        let name = element.firstName + " " + element.lastName
-        let bio = element.bio
-        let avatar = element.avatar
+      for (let index = 0; index < data.length; index++) {
+          const element = data[index];
+          let name = element.firstName + " " + element.lastName
+          let bio = element.bio
+          let avatar = element.avatar
 
-        let card = $("<div>").attr({
-            authorId: element.id,
-            class: "card custom_card author_card"
-        })
+          let card = $("<div>").attr({
+              authorId: element.id,
+              class: "card custom_card author_card"
+          })
 
-        if (avatar === null) {
-            let rand = Math.floor(Math.random() * 10);
-            avatar = `https://avatars.dicebear.com/v2/human/${rand}.svg`
-        }
+          if (avatar === null) {
+              let rand = Math.floor(Math.random() * 10);
+              avatar = `https://avatars.dicebear.com/v2/human/${rand}.svg`
+          }
 
-        let avatarContainer = $("<div>")
-        let avatarImg = $("<img>").attr({
-            authorId: element.id,
-            class: "avatar",
-            src: avatar
-        })
-        avatarContainer.append(avatarImg)
+          let avatarContainer = $("<div>")
+          let avatarImg = $("<img>").attr({
+              authorId: element.id,
+              class: "avatar",
+              src: avatar
+          })
+          avatarContainer.append(avatarImg)
 
-        let authorInfo = $("<div>").addClass("author_info_container")
+          let authorInfo = $("<div>").addClass("author_info_container")
 
-        let authorName = $("<p>").attr({
-            authorId: element.id,
-        }).addClass("author_name").html(name);
+          let authorName = $("<p>").attr({
+              authorId: element.id,
+          }).addClass("author_name").html(name);
 
-        let authorBio = $("<p>").attr({
-            authorId: element.id,
-        }).addClass("author_bio").html(bio);
+          let authorBio = $("<p>").attr({
+              authorId: element.id,
+          }).addClass("author_bio").html(bio);
 
-        let btnAddBook = $("<button>").html("Add Book").addClass("btn btn-info add_book_btn").attr({
-            author_id: element.id
-        })
+          let btnAddBook = $("<button>").html("Add Book").addClass("btn btn-info add_book_btn").attr({
+              author_id: element.id
+          })
 
-        let btnDeleteAuthor = $("<button>").html("Remove Author").addClass("btn delete-authors-btn btn-danger").attr({
-            author_id: element.id
-        })
+          let btnDeleteAuthor = $("<button>").html("Remove Author").addClass("btn delete-authors-btn btn-danger").attr({
+              author_id: element.id
+          })
 
-        authorInfo.append([authorName, authorBio]);
+          authorInfo.append([authorName, authorBio]);
 
-        card.append([avatarContainer, authorInfo, btnAddBook, btnDeleteAuthor])
+          card.append([avatarContainer, authorInfo, btnAddBook, btnDeleteAuthor])
 
-        $("#card__contain").append(card)
-    }
-})
+          $("#card__contain").append(card)
+      }
+  })
 
-// create search ussing ?term= 
-$("body").on("submit", "#add_book_form", (event) => {
-    event.preventDefault()
-    //get the values from the input feilds
-    let title = $("#title").val()
+  // create search ussing ?term= 
+  $("body").on("submit", "#add_book_form", (event) => {
+      event.preventDefault()
+      //get the values from the input feilds
+      let title = $("#title").val()
 
-    let isbn = $("#isbn").val()
-    let author_id = $("#submit_btn").attr("author_id")
+      let isbn = $("#isbn").val()
+      let author_id = $("#submit_btn").attr("author_id")
 
-    //send this off to the database
-    $.post(API + "/books", {
-        title,
-        isbn,
-        author_id
-    }, function (response) {
-
-
-        createAlert("#add_book_form");
-        if (response.success) {
-            $("#isbn").val("")
-            $("#title").val("")
-
-            $("#alert").addClass("alert-success")
-            $("#alertmsg").text(response.success)
-
-        } else {
-            $("#alert").addClass("alert-danger")
-            $("#alertmsg").text(response.error)
-        }
+      //send this off to the database
+      $.post(API + "/books", {
+          title,
+          isbn,
+          author_id
+      }, function (response) {
 
 
-        console.log(response)
-    })
-})
-// 
-$("body").on("submit", "#searchForm", (event) => {
+          createAlert("#add_book_form");
+          if (response.success) {
+              $("#isbn").val("")
+              $("#title").val("")
 
-    let bookUrl = API + "/search"
-    $.search(bookUrl, function (response) {
-        event.preventDefault()
-        let data = response.results
+              $("#alert").addClass("alert-success")
+              $("#alertmsg").text(response.success)
 
-        let search = $("#searchForm").val()
+          } else {
+              $("#alert").addClass("alert-danger")
+              $("#alertmsg").text(response.error)
+          }
 
-        createBookCards(search)
-    })
 
-})
+          console.log(response)
+      })
+  })
+  // 
+  $("body").on("submit", "#searchForm", (event) => {
 
-// When an author card is clicked, go get the books written by that author and console log them
-$("body").on("click", "#card__contain", function (event) {
+      let bookUrl = API + "/search"
+      $.search(bookUrl, function (response) {
+          event.preventDefault()
+          let data = response.results
 
-    let target = $(event.target)
-    let authorId = target.attr("authorId")
+          let search = $("#searchForm").val()
 
-    let authorBooksUrl = API + `/books/${authorId}`
+          createBookCards(search)
+      })
 
-    $.get(authorBooksUrl, function (response) {
-        let data = response.result
-        createBookCards(data)
-    })
-})
+  })
 
-// delete a book when this button is clicked
-// $.ajax({
-//     url,
-//     type: 'DELETE'/"PUT",
-//     success: (result) => {
-//         console.log(result)
-//     }
-// });
-$("body").on("click", ".delete-book-btn", (event) => {
-    let bookId = $(event.target).attr("bookId")
-    let url = API + `/books/${bookId}`
-    $.ajax({
-        url,
-        type: 'DELETE',
-        success: (results) => {
-            console.log(results)
-            if (results.success) {
-                $(event.target).parent().hide()
-            }
-        }
-    });
-})
+  // When an author card is clicked, go get the books written by that author and console log them
+  $("body").on("click", "#card__contain", function (event) {
 
-function createBookCards(books) {
-    // Clear the books first
-    $('#books').empty();
-    if (books === undefined) {
-        return
-    }
+      let target = $(event.target)
+      let authorId = target.attr("authorId")
 
-    // loop over the books array
-    // Create a book card for each of them
-    for (let index = 0; index < books.length; index++) {
-        const element = books[index];
-        let {
-            id,
-            title,
-            isbn,
-            author_id
-        } = element
+      let authorBooksUrl = API + `/books/${authorId}`
 
-        let deleteBtn = $("<button>").attr({
-            bookId: id,
-            class: "btn btn-danger delete-btn delete-book-btn"
-        }).html("Delete")
-        let card = $("<div>").addClass("card custom_card book_card");
-        title = $('<p>').html(title)
-        $(card).append([title, deleteBtn])
-        $('#books').append(card)
-    }
-}
+      $.get(authorBooksUrl, function (response) {
+          let data = response.result
+          createBookCards(data)
+      })
+  })
 
-// GOAL - when button is clicked create new form
-// create a on click event for when the user clicks addBookBtn
-$("body").on("click", ".add_book_btn", function (event) {
-    let authorid = $(event.target).attr("author_id")
+  // delete a book when this button is clicked
+  // $.ajax({
+  //     url,
+  //     type: 'DELETE'/"PUT",
+  //     success: (result) => {
+  //         console.log(result)
+  //     }
+  // });
+  $("body").on("click", ".delete-book-btn", (event) => {
+      let bookId = $(event.target).attr("bookId")
+      let url = API + `/books/${bookId}`
+      $.ajax({
+          url,
+          type: 'DELETE',
+          success: (results) => {
+              console.log(results)
+              if (results.success) {
+                  $(event.target).parent().hide()
+              }
+          }
+      });
+  })
 
-    let formTemplate = $(`<form id="add_book_form" style="width:100%">
+  function createBookCards(books) {
+      // Clear the books first
+      $('#books').empty();
+      if (books === undefined) {
+          return
+      }
+
+      // loop over the books array
+      // Create a book card for each of them
+      for (let index = 0; index < books.length; index++) {
+          const element = books[index];
+          let {
+              id,
+              title,
+              isbn,
+              author_id
+          } = element
+
+          let deleteBtn = $("<button>").attr({
+              bookId: id,
+              class: "btn btn-danger delete-btn delete-book-btn"
+          }).html("Delete")
+          let card = $("<div>").addClass("card custom_card book_card");
+          title = $('<p>').html(title)
+          $(card).append([title, deleteBtn])
+          $('#books').append(card)
+      }
+  }
+
+  // GOAL - when button is clicked create new form
+  // create a on click event for when the user clicks addBookBtn
+  $("body").on("click", ".add_book_btn", function (event) {
+      let authorid = $(event.target).attr("author_id")
+
+      let formTemplate = $(`<form id="add_book_form" style="width:100%">
     <div class="form-group">
       <label for="title">Book Title</label>
       <input type="text" class="form-control" id="title" minlength="1" maxlength="100" placeholder="Enter title" required="false">
@@ -215,63 +215,63 @@ $("body").on("click", ".add_book_btn", function (event) {
   </form>`)
 
 
-    // append it to the author card
-    let authorCard = $(event.target).parent()
+      // append it to the author card
+      let authorCard = $(event.target).parent()
 
-    authorCard.append(formTemplate)
-})
+      authorCard.append(formTemplate)
+  })
 
-// create a book when the user clicks the Add Book btn
-$("body").on("submit", "#add_book_form", (event) => {
-    event.preventDefault()
-    //get the values from the input feilds
-    let title = $("#title").val()
+  // create a book when the user clicks the Add Book btn
+  $("body").on("submit", "#add_book_form", (event) => {
+      event.preventDefault()
+      //get the values from the input feilds
+      let title = $("#title").val()
 
-    let isbn = $("#isbn").val()
-    let author_id = $("#submit_btn").attr("author_id")
+      let isbn = $("#isbn").val()
+      let author_id = $("#submit_btn").attr("author_id")
 
-    //send this off to the database
-    $.post(API + "/books", {
-        title,
-        isbn,
-        author_id
-    }, function (response) {
-
-
-        createAlert("#add_book_form");
-        if (response.success) {
-            $("#isbn").val("")
-            $("#title").val("")
-
-            $("#alert").addClass("alert-success")
-            $("#alertmsg").text(response.success)
-
-        } else {
-            $("#alert").addClass("alert-danger")
-            $("#alertmsg").text(response.error)
-        }
+      //send this off to the database
+      $.post(API + "/books", {
+          title,
+          isbn,
+          author_id
+      }, function (response) {
 
 
-        console.log(response)
-    })
-})
+          createAlert("#add_book_form");
+          if (response.success) {
+              $("#isbn").val("")
+              $("#title").val("")
 
-function createAlert(alertContainer) {
-    let alert = $(`<div id="alert" class="alert alert-dismissible fade show" role="alert">
+              $("#alert").addClass("alert-success")
+              $("#alertmsg").text(response.success)
+
+          } else {
+              $("#alert").addClass("alert-danger")
+              $("#alertmsg").text(response.error)
+          }
+
+
+          console.log(response)
+      })
+  })
+
+  function createAlert(alertContainer) {
+      let alert = $(`<div id="alert" class="alert alert-dismissible fade show" role="alert">
     <p id="alertmsg"></p>
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
   </div>`)
 
-    $(alertContainer).prepend(alert)
-}
+      $(alertContainer).prepend(alert)
+  }
 
-// GOAL - create new author form
-// create a on click event for when the user clicks author-btn
-$("body").on("click", ".author-btn", function (event) {
+  // GOAL - create new author form
+  // create a on click event for when the user clicks author-btn
+  $("body").on("click", ".author-btn", function (event) {
 
-    let formTemplate = $(`<form id="add_author_form">
+      let formTemplate = $(`<form id="add_author_form">
     <div class="form-group">
       <label for="Author first Name">First name</label>
       <input type="text" class="form-control" id="firstName" minlength="1" maxlength="100" placeholder="Enter first name" required>
@@ -294,62 +294,62 @@ $("body").on("click", ".author-btn", function (event) {
   </form>`)
 
 
-    // append it to the author card
-    let addAuthorbutton = $(event.target).parent()
+      // append it to the author card
+      let addAuthorbutton = $(event.target).parent()
 
-    addAuthorbutton.append(formTemplate)
+      addAuthorbutton.append(formTemplate)
 
 
-})
+  })
 
-// create a author when the user clicks the Add Author btn
-$("body").on("submit", "#add_author_form", (event) => {
+  // create a author when the user clicks the Add Author btn
+  $("body").on("submit", "#add_author_form", (event) => {
 
-    event.preventDefault()
-    //get the values from the input feilds
-    let firstName = $("#firstName").val()
-    let lastName = $("#lastName").val()
-    let bio = $("#bio").val()
-    let avatar = $("#avatar").val()
+      event.preventDefault()
+      //get the values from the input feilds
+      let firstName = $("#firstName").val()
+      let lastName = $("#lastName").val()
+      let bio = $("#bio").val()
+      let avatar = $("#avatar").val()
 
-    //send this off to the database
-    $.post(API + "/authors", {
-        firstName,
-        lastName,
-        bio,
-        avatar
-    }, function (response) {
+      //send this off to the database
+      $.post(API + "/authors", {
+          firstName,
+          lastName,
+          bio,
+          avatar
+      }, function (response) {
 
-        createAlert("#add_author_form");
-        if (response.success) {
-            $("#firstName").val("")
-            $("#lastName").val("")
-            $("#bio").val("")
-            $("#avatar").val("")
+          createAlert("#add_author_form");
+          if (response.success) {
+              $("#firstName").val("")
+              $("#lastName").val("")
+              $("#bio").val("")
+              $("#avatar").val("")
 
-            $("#alert").addClass("alert-success")
-            $("#alertmsg").text(response.success)
+              $("#alert").addClass("alert-success")
+              $("#alertmsg").text(response.success)
 
-        } else {
-            $("#alert").addClass("alert-danger")
-            $("#alertmsg").text(response.error)
-        }
+          } else {
+              $("#alert").addClass("alert-danger")
+              $("#alertmsg").text(response.error)
+          }
 
-    })
-})
+      })
+  })
 
-// delete a author when the user clicks the delete Author btn
-$("body").on("click", ".delete-authors-btn", (event) => {
-    let authorId = $(event.target).attr("author_id")
-    let url = API + `/authors/${authorId}`
-    $.ajax({
-        url,
-        type: 'DELETE',
-        success: (result) => {
-            console.log(result)
-            if (result.success) {
-                $(event.target).parent().hide()
-            }
-        }
-    });
-})
+  // delete a author when the user clicks the delete Author btn
+  $("body").on("click", ".delete-authors-btn", (event) => {
+      let authorId = $(event.target).attr("author_id")
+      let url = API + `/authors/${authorId}`
+      $.ajax({
+          url,
+          type: 'DELETE',
+          success: (result) => {
+              console.log(result)
+              if (result.success) {
+                  $(event.target).parent().hide()
+              }
+          }
+      });
+  })
